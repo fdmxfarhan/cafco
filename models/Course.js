e59ac1a7e4a71598da66928fe52bfb14495207ad
Farhan Daemi <fdmxfarhan@yahoo.com>
2021-06-09 22:05:32 +0430

    asdf

--- models/Course.js
+++ models/Course.js
@@ -1,28 +1,27 @@
 var mongoose = require('mongoose');
 
 var CourseSchema = new mongoose.Schema({
-  price: Number, // Rial
-  discount: Number, //percent
-  haveDiscount: Boolean,
-  title: String, 
-  description: String,
-  teacher: String,
-  startDate: Object,
-  session: Number,
-  requiredCourse: [Object],
-  requiredTools: [Object],
-  minAge: Number,
-  maxAge: Number,
-  status: {
-    type: String,
-    default: 'شروع نشده'
-  },
-  capacity: Number,
-  cover: String,
+    price: Number, // Rial
+    discount: Number, //percent
+    haveDiscount: Boolean,
+    title: String,
+    description: String,
+    teacher: String,
+    startDate: Object,
+    endDate: Object,
+    session: Number,
+    requiredCourse: [Object],
+    requiredTools: [Object],
+    minAge: Number,
+    maxAge: Number,
+    status: {
+        type: String,
+        default: 'شروع نشده'
+    },
+    capacity: Number,
+    cover: String,
 });
 
 var Course = mongoose.model('Course', CourseSchema);
 
 module.exports = Course;
-
-
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -10,14 +10,150 @@ const dot = require('../config/dot');
 const shamsi = require('../config/shamsi');
 
 
+
+function div(a, b) {
+    return parseInt((a / b));
+}
+
+function gregorian_to_jalali(g_y, g_m, g_d) {
+    var g_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var j_days_in_month = [31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29];
+    var jalali = [];
+    var gy = g_y - 1600;
+    var gm = g_m - 1;
+    var gd = g_d - 1;
+
+    var g_day_no = 365 * gy + div(gy + 3, 4) - div(gy + 99, 100) + div(gy + 399, 400);
+
+    for (var i = 0; i < gm; ++i)
+        g_day_no += g_days_in_month[i];
+    if (gm > 1 && ((gy % 4 == 0 && gy % 100 != 0) || (gy % 400 == 0)))
+    /* leap and after Feb */
+        g_day_no++;
+    g_day_no += gd;
+
+    var j_day_no = g_day_no - 79;
+
+    var j_np = div(j_day_no, 12053);
+    /* 12053 = 365*33 + 32/4 */
+    j_day_no = j_day_no % 12053;
+
+    var jy = 979 + 33 * j_np + 4 * div(j_day_no, 1461);
+    /* 1461 = 365*4 + 4/4 */
+
+    j_day_no %= 1461;
+
+    if (j_day_no >= 366) {
+        jy += div(j_day_no - 1, 365);
+        j_day_no = (j_day_no - 1) % 365;
+    }
+    for (var i = 0; i < 11 && j_day_no >= j_days_in_month[i]; ++i)
+        j_day_no -= j_days_in_month[i];
+    var jm = i + 1;
+    var jd = j_day_no + 1;
+    jalali[0] = jy;
+    jalali[1] = jm;
+    jalali[2] = jd;
+    return jalali;
+    //return jalali[0] + "_" + jalali[1] + "_" + jalali[2];
+    //return jy + "/" + jm + "/" + jd;
+}
+
+function get_year_month_day(date) {
+    var convertDate;
+    var y = date.substr(0, 4);
+    var m = date.substr(5, 2);
+    var d = date.substr(8, 2);
+    convertDate = gregorian_to_jalali(y, m, d);
+    return convertDate;
+}
+
+function get_hour_minute_second(time) {
+    var convertTime = [];
+    convertTime[0] = time.substr(0, 2);
+    convertTime[1] = time.substr(3, 2);
+    convertTime[2] = time.substr(6, 2);
+    return convertTime;
+}
+
+function convertDate(date) {
+    var convertDateTime = get_year_month_day(date.substr(0, 10));
+    convertDateTime = convertDateTime[0] + "/" + convertDateTime[1] + "/" + convertDateTime[2] + " " + date.substr(10);
+    return convertDateTime;
+}
+
+function get_persian_month(month) {
+    switch (month) {
+        case 1:
+            return "فروردین";
+            break;
+        case 2:
+            return "اردیبهشت";
+            break;
+        case 3:
+            return "خرداد";
+            break;
+        case 4:
+            return "تیر";
+            break;
+        case 5:
+            return "مرداد";
+            break;
+        case 6:
+            return "شهریور";
+            break;
+        case 7:
+            return "مهر";
+            break;
+        case 8:
+            return "آبان";
+            break;
+        case 9:
+            return "آذر";
+            break;
+        case 10:
+            return "دی";
+            break;
+        case 11:
+            return "بهمن";
+            break;
+        case 12:
+            return "اسفند";
+            break;
+    }
+}
+setInterval(() => {
+    greg = new Date(Date.now());
+    day = greg.getDate();
+    month = greg.getMonth() + 1;
+    year = greg.getFullYear();
+    jalali = gregorian_to_jalali(year, month, day)
+    now = { year: jalali[0], month: jalali[1], day: jalali[2] };
+    // console.log({ year, month, day });
+    // console.log(now);
+    Course.find({}, (err, courses) => {
+        courses.forEach(course => {
+            // console.log(course.endDate, now);
+            if (parseInt(course.startDate.day) <= now.day && parseInt(course.startDate.month) <= now.month && parseInt(course.startDate.year) <= now.year && course.status == 'شروع نشده') {
+                Course.updateMany({ _id: course._id }, { $set: { status: 'در حال برگزاری' } }, (err, doc) => {
+                    if (err) console.log(err);
+                });
+            }
+            if (parseInt(course.endDate.day) <= now.day && parseInt(course.endDate.month) <= now.month && parseInt(course.endDate.year) <= now.year && course.status == 'در حال برگزاری') {
+                Course.updateMany({ _id: course._id }, { $set: { status: 'پایان یافته' } }, (err, doc) => {
+                    if (err) console.log(err);
+                });
+            }
+        });
+    });
+}, 30 * 1000);
+
 router.get('/', ensureAuthenticated, (req, res, next) => {
-    if(req.user.role == 'user')
-    {
+    if (req.user.role == 'user') {
         Course.find({}, (err, courses) => {
             var notPayedCoursesNum = 0;
-            for(var i=0; i<req.user.course.length; i++)
-            {
-                if(!req.user.course[i].payed) notPayedCoursesNum++;
+            for (var i = 0; i < req.user.course.length; i++) {
+                if (!req.user.course[i].payed) notPayedCoursesNum++;
             }
             res.render('./dashboard/user-dashboard', {
                 user: req.user,
@@ -27,15 +163,13 @@ router.get('/', ensureAuthenticated, (req, res, next) => {
                 dot,
             });
         });
-    }
-    else if(req.user.role = 'admin')
-    {
+    } else if (req.user.role = 'admin') {
         Course.find({}, (err, courses) => {
             User.find({}, (err, users) => {
                 var studentsNum = 0;
                 for (let i = 0; i < users.length; i++) {
                     users[i].course.forEach(userCourse => {
-                        if(userCourse.payed) studentsNum++;
+                        if (userCourse.payed) studentsNum++;
                     });
                 }
                 res.render('./dashboard/admin-dashboard', {
@@ -51,39 +185,38 @@ router.get('/', ensureAuthenticated, (req, res, next) => {
 });
 
 router.get('/users-view', ensureAuthenticated, (req, res, next) => {
-    if(req.user.role == 'admin'){
+    if (req.user.role == 'admin') {
         User.find({}, (err, users) => {
             res.render('./dashboard/admin-users-view', {
                 user: req.user,
                 users,
             });
         });
-    }
-    else
+    } else
         res.send('permission denied');
 });
 
 router.get('/register-course', ensureAuthenticated, (req, res, next) => {
     Course.findById(req.query.courseID, (err, course) => {
-        if(err) console.log(err);
+        if (err) console.log(err);
         var courseList = req.user.course;
         var registered = false;
         courseList.forEach(course2 => {
-            if(course2.courseID == req.query.courseID) registered = true;
+            if (course2.courseID == req.query.courseID) registered = true;
         });
-        if(!registered){
-            courseList.push({courseID: req.query.courseID, course, payed: false});
-            User.updateMany({idNumber: req.user.idNumber}, {$set: {course: courseList}}, (err, doc) => {
+        if (!registered) {
+            courseList.push({ courseID: req.query.courseID, course, payed: false });
+            User.updateMany({ idNumber: req.user.idNumber }, { $set: { course: courseList } }, (err, doc) => {
                 res.redirect('/dashboard');
             });
-        }else{
+        } else {
             res.send('این دوره قبلا ثبت شده');
         }
     })
 });
 
 router.get('/remove-course', ensureAuthenticated, (req, res, next) => {
-    Course.deleteOne({_id: req.query.courseID}, (err, doc) => {
+    Course.deleteOne({ _id: req.query.courseID }, (err, doc) => {
         res.redirect('/dashboard');
     });
 });
@@ -91,14 +224,14 @@ router.get('/remove-course', ensureAuthenticated, (req, res, next) => {
 router.get('/remove-user-course', ensureAuthenticated, (req, res, next) => {
     var courseList = req.user.course;
     courseList.splice(req.query.index, 1);
-    User.updateMany({idNumber: req.user.idNumber}, {$set: {course: courseList}}, (err, doc) => {
+    User.updateMany({ idNumber: req.user.idNumber }, { $set: { course: courseList } }, (err, doc) => {
         res.redirect('/dashboard');
     });
 });
 router.get('/remove-user-course-pay', ensureAuthenticated, (req, res, next) => {
     var courseList = req.user.course;
     courseList.splice(req.query.index, 1);
-    User.updateMany({idNumber: req.user.idNumber}, {$set: {course: courseList}}, (err, doc) => {
+    User.updateMany({ idNumber: req.user.idNumber }, { $set: { course: courseList } }, (err, doc) => {
         res.redirect('/dashboard/pay');
     });
 });
@@ -106,9 +239,8 @@ router.get('/remove-user-course-pay', ensureAuthenticated, (req, res, next) => {
 router.get('/pay', ensureAuthenticated, (req, res, next) => {
     var courseList = req.user.course;
     var priceSum = 0;
-    for(var i=0; i<courseList.length; i++)
-    {
-        if(!courseList[i].payed)
+    for (var i = 0; i < courseList.length; i++) {
+        if (!courseList[i].payed)
             priceSum += courseList[i].course.price;
     }
     res.render('./dashboard/user-pay', {
@@ -120,8 +252,8 @@ router.get('/pay', ensureAuthenticated, (req, res, next) => {
 });
 
 router.get('/set-course-status', ensureAuthenticated, (req, res, next) => {
-    Course.updateMany({_id: req.query.courseID}, {$set: {status: req.query.status}}, (err, doc) => {
-        if(err) console.log(err);
+    Course.updateMany({ _id: req.query.courseID }, { $set: { status: req.query.status } }, (err, doc) => {
+        if (err) console.log(err);
         res.redirect('/dashboard');
     });
 });
@@ -149,7 +281,7 @@ router.get('/course-list', ensureAuthenticated, (req, res, next) => {
             var usersList = [];
             users.forEach(usr => {
                 usr.course.forEach(crs => {
-                    if(crs.courseID == req.query.courseID)
+                    if (crs.courseID == req.query.courseID)
                         usersList.push(usr);
                 });
             });
@@ -163,7 +295,7 @@ router.get('/course-list', ensureAuthenticated, (req, res, next) => {
 });
 
 router.get('/user-payments', ensureAuthenticated, (req, res, next) => {
-    Payment.find({idNumber: req.user.idNumber}, (err, payments) => {
+    Payment.find({ idNumber: req.user.idNumber }, (err, payments) => {
         res.render('./dashboard/user-payments', {
             payments,
             user: req.user,
@@ -171,5 +303,7 @@ router.get('/user-payments', ensureAuthenticated, (req, res, next) => {
         });
     });
 });
+
+
 
 module.exports = router;
