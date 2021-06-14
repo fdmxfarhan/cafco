4cb454da99f0dd81697e67403b2ac0425ae98383
fdmxfarhan <fdmxfarhan@yahoo.com>
2021-06-14 16:39:02 +0430

    m

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -23,20 +23,10 @@ $(document).ready(function(){
         for(var i=0;i<100; i++)
             $(`#course-view${i}`).fadeOut(500);
     });
-    var cards = [
-        {card: $('#card0'), view: $('#course-view0')},
-        {card: $('#card1'), view: $('#course-view1')},
-        {card: $('#card2'), view: $('#course-view2')},
-        {card: $('#card3'), view: $('#course-view3')},
-        {card: $('#card4'), view: $('#course-view4')},
-        {card: $('#card5'), view: $('#course-view5')},
-        {card: $('#card6'), view: $('#course-view6')},
-        {card: $('#card7'), view: $('#course-view7')},
-        {card: $('#card8'), view: $('#course-view8')},
-        {card: $('#card9'), view: $('#course-view9')},
-        {card: $('#card10'), view: $('#course-view10')},
-        {card: $('#card11'), view: $('#course-view11')},
-    ];
+    var cards = [];
+    for(var i=0; i<500; i++){
+        cards.push({card: $(`#card${i}`), view: $(`#course-view${i}`)});
+    }
     cards.forEach(card => {
         card.card.click(() => {
             card.view.fadeIn(500);
