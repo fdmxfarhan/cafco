a4b56977008eb27c7d98a6b730a15ae9cde8a187
Farhan Daemi <fdmxfarhan@yahoo.com>
2021-06-19 11:46:24 +0430

    adf

--- models/Payment.js
+++ models/Payment.js
@@ -1,22 +1,22 @@
 var mongoose = require('mongoose');
 
 var PaymentSchema = new mongoose.Schema({
-    userID: String,
-    fullname: String,
-    idNumber: String,
-    phone: Number,
-    amount: Number,
-    description: String,
-    date: Date,
-    payDate: Date,
-    payed: {
-      type: Boolean,
-      default: false
-    },
-    track_id: String,
-    courseList: [Object],
-    discount: Number,
-  });
+  userID: String,
+  fullname: String,
+  idNumber: String,
+  phone: Number,
+  amount: Number,
+  description: String,
+  date: Date,
+  payDate: Date,
+  payed: {
+    type: Boolean,
+    default: false,
+  },
+  track_id: String,
+  courseList: [Object],
+  discount: Number,
+});
 
 var Payment = mongoose.model('Payment', PaymentSchema);
 
