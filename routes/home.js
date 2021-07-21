0f625ef890369542bf08ee006cda126a4329ca29
Farhan Daemi <fdmxfarhan@yahoo.com>
2021-07-21 18:41:05 +0430

    asd

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
+    console.log(req.url)
     res.redirect('/users/login');
     // res.render('home');
 });
